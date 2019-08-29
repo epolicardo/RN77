2df4e992abf6f97c3196c70366c6156f536e1b79
Gaston Cordeiro <43023297+GastonCordeiro@users.noreply.github.com>
2019-08-29 12:00:45 -0300

    Add Controlador Persona
    Add Controlador PersonaDocumentos, Tdocumentos y Documentos.

---
+++ RN77/RN77.Actores/Controllers/PersonaController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RN77.BD.Datos;
+using RN77.BD.Datos.Entities;
+
+namespace RN77.Actores.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PersonasController : ControllerBase
+	{
+		private readonly RN77Context _context;
+
+		public PersonasController(RN77Context context)
+		{
+			_context = context;
+		}
+
+		// GET: api/Personas
+
+		[HttpGet]
+
+		public async Task<ActionResult<IEnumerable<Personas>>> GetPersonas()
+
+		{
+
+			return await _context.Personas.ToListAsync();
+
+		}
+
+
+		// GET: api/Personas
+
+		[HttpGet("{id}")]
+
+		public async Task<ActionResult<Personas>> GetPersonas(long id)
+
+		{
+
+			var personasItem = await _context.Personas.FindAsync(id);
+
+
+
+			if (personasItem == null)
+
+			{
+
+				return NotFound();
+
+			}
+
+
+
+			return personasItem;
+
+		}
+
+
+
+		// POST: api/Personas
+
+		[HttpPost]
+
+		public async Task<ActionResult<Personas>> PostPersonas(Personas item)
+
+		{
+
+			_context.Personas.Add(item);
+
+			await _context.SaveChangesAsync();
+
+
+
+			return CreatedAtAction(nameof(GetPersonas), new { id = item.Id }, item);
+
+		}
+
+
+
+		// PUT: api/Personas/5
+
+
+
+		[HttpPut("{id}")]
+
+		public async Task<IActionResult> PutPersonas(long id, Personas item)
+
+		{
+
+			if (id != item.Id)
+
+			{
+
+				return BadRequest();
+
+			}
+
+
+
+			_context.Entry(item).State = EntityState.Modified;
+
+			await _context.SaveChangesAsync();
+
+
+
+			return NoContent();
+
+		}
+
+
+
+		// DELETE: api/personas/5
+
+		[HttpDelete("{id}")]
+
+		public async Task<IActionResult> DeletePersonas(long id)
+
+		{
+
+			var personas = await _context.Personas.FindAsync(id);
+
+
+
+			if (personas == null)
+
+			{
+
+				return NotFound();
+
+			}
+
+			_context.Personas.Remove(personas);
+
+			await _context.SaveChangesAsync();
+
+
+
+			return NoContent();
+
+		}
+
+	}
+}
---
+++ RN77/RN77.Actores/Controllers/PersonaMailsController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RN77.BD.Datos;
+using RN77.BD.Datos.Entities;
+
+namespace RN77.Actores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaMailsController : ControllerBase
+    {
+
+		private readonly RN77Context _context;
+
+		public PersonaMailsController(RN77Context context)
+		{
+			_context = context;
+		}
+
+		// GET: api/PersonaMails
+
+		[HttpGet]
+
+		public async Task<ActionResult<IEnumerable<PersonaMails>>> GetPersonaMails()
+
+		{
+
+			return await _context.PersonaMails.ToListAsync();
+
+		}
+
+		// GET: api/PersonaMails
+
+		[HttpGet("{id}")]
+
+		public async Task<ActionResult<PersonaMails>> GetPersonaMails(long id)
+
+		{
+
+			var personaMailsItem = await _context.PersonaMails.FindAsync(id);
+
+
+
+			if (personaMailsItem == null)
+
+			{
+
+				return NotFound();
+
+			}
+
+
+
+			return personaMailsItem;
+
+		}
+
+
+
+		// POST: api/Personas
+
+		[HttpPost]
+
+		public async Task<ActionResult<PersonaDomicilios>> PostPersonaMails(PersonaMails item)
+
+		{
+
+			_context.PersonaMails.Add(item);
+
+			await _context.SaveChangesAsync();
+
+
+
+			return CreatedAtAction(nameof(GetPersonaMails), new { id = item.Id }, item);
+
+		}
+
+
+
+		// PUT: api/PersonaDomicilios
+
+
+
+		[HttpPut("{id}")]
+
+		public async Task<IActionResult> PutPersonaMails(long id, PersonaMails item)
+
+		{
+
+			if (id != item.Id)
+
+			{
+
+				return BadRequest();
+
+			}
+
+
+
+			_context.Entry(item).State = EntityState.Modified;
+
+			await _context.SaveChangesAsync();
+
+
+
+			return NoContent();
+
+		}
+
+
+
+		// DELETE: api/Domicilios/5
+
+		[HttpDelete("{id}")]
+
+		public async Task<IActionResult> DeletePersonaMails(long id)
+
+		{
+
+			var personaMails = await _context.PersonaMails.FindAsync(id);
+
+
+
+			if (personaMails == null)
+
+			{
+
+				return NotFound();
+
+			}
+
+
+
+			_context.PersonaMails.Remove(personaMails);
+
+			await _context.SaveChangesAsync();
+
+
+
+			return NoContent();
+
+		}
+	}
+}
