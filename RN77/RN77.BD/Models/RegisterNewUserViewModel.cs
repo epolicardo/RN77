0a9fbd92e8bc25bb98c86a0fa1a95a3ed7b4bb08
Octavio Zalazar <octizalazar99@gmail.com>
2019-09-09 12:10:36 -0300

    Added CuentaController and View Models

---
+++ RN77/RN77.BD/Models/RegisterNewUserViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RN77.BD.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class RegisterNewUserViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string Nombre { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string Apellido { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Username { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string Confirm { get; set; }
+    }
+
+}
