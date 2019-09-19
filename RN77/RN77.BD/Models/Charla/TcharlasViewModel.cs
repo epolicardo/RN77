087d76bbc6b3156f5c5c8d47822a5d072a414c55
santiagoolmedo <50325991+santiagoolmedo@users.noreply.github.com>
2019-09-19 13:24:04 -0300

    Add controller Charlas, Tcharlas ,CharlaPersonas
    con su viewmodel cada uno

---
+++ RN77/RN77.BD/Models/Charla/TcharlasViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RN77.BD.Models.Charla
+{
+    public class TcharlasViewModel
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
---
+++ RN77/RN77.Charla/Controllers/CharlaPersonasController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RN77.BD.Controllers;
+using RN77.BD.Datos;
+using RN77.BD.Datos.Entities;
+using RN77.BD.Models.Charla;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+//Olmedo Santiago
+
+
+namespace RN77.Charla.Personas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharlaPersonasController : ControllerBase
+    {
+        private readonly RN77Context context;
+        public CharlaPersonasController(RN77Context context)
+        {
+            this.context = context;
+        }
+        // GET: api/CharlaPersona
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CharlaPersonas>>> GetCharlaPersonas()
+        {
+            return await context.CharlaPersonas.ToListAsync();
+        }
+
+        // GET api/Tcharlas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CharlaPersonas>> GetCharlaPersonas(int id)
+        {
+            var CharlaPersonasItem = await context.CharlaPersonas.FindAsync(id);
+            if (CharlaPersonasItem == null)
+            {
+                return this.BadRequest("No existe tal Charla");
+            }
+            return CharlaPersonasItem;
+        }
+
+        // POST api/Tcharlas
+        [HttpPost]
+        public async Task<IActionResult> PostCharlaPersonas([FromBody]CharlaPersonasViewModel CharlaPersonasViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return this.BadRequest(ModelState);
+            }
+            var user = await this.context.Users.FindAsync("1");
+            if (user == null)
+            {
+                return this.BadRequest("Usuario Invalido");
+            }
+            var entity = new CharlaPersonas
+            {
+                CharlaId = CharlaPersonasViewModel.CharlaId,
+                PersonaId = CharlaPersonasViewModel.PersonaId,
+            };
+            BaseController.CompletaRegistro(entity, 1, "", user, false);
+
+
+            await this.context.Set<CharlaPersonas>().AddAsync(entity);
+            try
+            {
+                await this.context.SaveChangesAsync();
+            }
+            catch (Exception ee)
+            {
+                return this.BadRequest("Registro no grabado, controlar.");
+            }
+            return Ok(entity);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
