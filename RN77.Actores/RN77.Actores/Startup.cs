98e93b9f49242636ddc275e9dd4206439ead7567
Sergio <sergioalgorry@hotmail.com>
2019-10-04 00:44:33 -0300

    Commit de consolidación

--- RN77.Actores/RN77.Actores/Startup.cs
+++ RN77.Actores/RN77.Actores/Startup.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RN77.BD.Datos;
+using RN77.BD.Datos.Entities;
+using RN77.BD.Helpers;
+using RN77.BD.Services;
 
 namespace RN77.Actores
 {
@@ -20,15 +24,43 @@ namespace RN77.Actores
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentity<Usuarios, IdentityRole>(cfg =>
+            {
+                cfg.Tokens.AuthenticatorTokenProvider = TokenOptions.DefaultAuthenticatorProvider;
+                cfg.SignIn.RequireConfirmedEmail = true;
+                cfg.User.RequireUniqueEmail = true;
+                cfg.Password.RequireDigit = false;
+                cfg.Password.RequiredUniqueChars = 0;
+                cfg.Password.RequireLowercase = false;
+                cfg.Password.RequireNonAlphanumeric = false;
+                cfg.Password.RequireUppercase = false;
+                cfg.Password.RequiredLength = 6;
+            }).AddDefaultTokenProviders().AddEntityFrameworkStores<RN77Context>(); 
+
+
+            //services.AddAuthentication()
+            //    .AddCookie()
+            //    .AddJwtBearer(cfg =>
+            //    {
+            //        cfg.TokenValidationParameters = new TokenValidationParameters
+            //        {
+            //            ValidIssuer = this.Configuration["Tokens:Issuer"],
+            //            ValidAudience = this.Configuration["Tokens:Audience"],
+            //            IssuerSigningKey = new SymmetricSecurityKey(
+            //                Encoding.UTF8.GetBytes(this.Configuration["Tokens:Key"]))
+            //        };
+            //    });
+
+
             services.AddDbContext<RN77Context>(cfg =>
             {
                 cfg.UseSqlServer(this.Configuration.GetConnectionString("RN77Connection"));
             });
 
             #region INYECCION
-            //services.AddScoped<IUsuarioHelper, UsuarioHelper>();
-            //services.AddScoped<IMailHelper, MailHelper>();
-            //services.AddScoped<IApiService, ApiService>();
+            services.AddScoped<IUsuarioHelper, UsuarioHelper>();
+            services.AddScoped<IMailHelper, MailHelper>();
+            services.AddScoped<IApiService, ApiService>();
             #endregion
 
             services.AddControllers();
