6d75017a320d11e80a40735640a6811dbd42988b
Sergio <sergioalgorry@hotmail.com>
2019-10-18 20:46:08 -0300

    Actualizado actores y charla

---
+++ RN77.Comun/RN77.BD/Controllers/InicioController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using RN77.Comun.Models.Varios;
+
+namespace RN77.BD.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InicioController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public InicioController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            var p = new InicioRequest(this.configuration["API"]);
+            return Ok(p.Mensaje);
+        }
+    }
+}
--- RN77.Comun/RN77.Comun/Models/Varios/InicioRequest.cs
+++ RN77.Comun/RN77.Comun/Models/Varios/InicioRequest.cs
@@ -1,15 +1,22 @@
+using Microsoft.Extensions.Configuration;
 using System;
 
-namespace RN77.Actores
+namespace RN77.Comun.Models.Varios
 {
     public class InicioRequest
     {
+        private readonly string api;
+
+        public InicioRequest(string api)
+        {
+            this.api = api;
+        }
         public string Mensaje
         {
             get
             {
                 return
-                $"    ......API Actores iniciada....." +
+                $"    ......API {this.api} iniciada....." +
                 $"    Fecha: {DateTime.Now}" +
                 $"    Consultar https://localhost:[port]/swagger";
             }
